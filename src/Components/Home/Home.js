a9592f5db135d6be245de49be8fa1c7e1e0e2de9
Rayeem Omayeer <rayeemomayeer@gmail.com>
2021-11-10 06:36:00 +0600

    topbanner and navbar added

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar/Navbar';
+import TopBanner from './TopBanner/TopBanner';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <TopBanner></TopBanner>
+    </div>
+  );
+};
+
+export default Home;
